10e90b064782a500e0c3b5538a8990e64ae11162
Edoardo Grassi <edo.g@hotmail.it>
2023-09-11 12:21:45 +0200

    feat: added platform specific projects

---
+++ TsMap.Canvas.Gtk/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Eto.Forms;
+
+namespace TsMap.Canvas.Gtk
+{
+    class MainClass
+    {
+        [STAThread]
+        public static void Main(string[] args)
+        {
+            new Application(Eto.Platforms.Gtk).Run(new SetupForm());
+        }
+    }
+}
---
+++ TsMap.Canvas.WinForms/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Eto.Forms;
+
+namespace TsMap.Canvas.Winforms
+{
+    class MainClass
+    {
+        [STAThread]
+        public static void Main(string[] args)
+        {
+            new Application(Eto.Platforms.WinForms).Run(new SetupForm());
+        }
+    }
+}
